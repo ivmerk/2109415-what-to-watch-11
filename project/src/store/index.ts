44c853cc9f20b437b0b04597e0a0cc28b92325b8
ivmerk <ivan.e.merkulov@gmail.com>
2022-11-27 12:58:26 +0200

    7.16.1

--- project/src/store/index.ts
+++ project/src/store/index.ts
@@ -1,13 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createAPI } from '../services/api';
 import { updateStore } from './reducer';
-import { createAPI } from '../services/api';
 
-<<<<<<< HEAD
 export const api = createAPI();
-=======
-export const api = create
->>>>>>> 68ba663 (something)
 
 export const store = configureStore({
   reducer:updateStore,
